12882156f38ad43ea3f2b6664ea707a20ce8eb35
N Okunev <nokunevs@gmail.com>
2018-08-13 16:56:53 +0300

    first lesson   servlets & JSP

--- src/main/java/lv/ctco/javaschool/firstweb/HelloWorldServlet.java
+++ src/main/java/lv/ctco/javaschool/firstweb/HelloWorldServlet.java
@@ -12,11 +12,18 @@ import java.io.PrintWriter;
 public class HelloWorldServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        try (PrintWriter out = response.getWriter()){
+            if ("12345".equals(password)) {
+                out.append("<h1>You are logged in, "+username+"</h1>");
+            } else {
+                out.append("<h1>Wrong password!</h1>");
+            }
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try (PrintWriter out = response.getWriter()) {
-            out.append("Hello, World, from the servlet!");
-        }
     }
+
 }
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,26 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: olga.polinkina01
+  Date: 8/13/2018
+  Time: 16:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+<%
+    for(int i=0; i<10; i++){
+%>
+    <h1> Hello <%=i%></h1>
+
+<%
+    }
+%>
+
+
+</body>
+</html>
